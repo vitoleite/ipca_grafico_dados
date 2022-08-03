afc5169f46b5329f1b444c8eaf9b74e309bb0cec
vitoleite <vitudhr@gmail.com>
2022-08-02 21:44:32 -0300

    Organizando projeto

---
+++ ipca/source/transform.py
@@ -0,0 +1,134 @@
+import os
+from pathlib import Path
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+import wget
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class dataFormat:
+    def __init__(self):
+        self.url = "https://sidra.ibge.gov.br/geratabela?format=us.csv&name=tabela1737.csv&terr=N&rank=-&query=t/1737/n1/all/v/all/p/all/d/v63%202,v69%202,v2263%202,v2264%202,v2265%202,v2266%2013/l/,v,p%2Bt"
+        self.ano_mes = datetime.today().date()
+        self.ano_mes = self.ano_mes + relativedelta(months=-1)
+        self.ano_mes_formatado = self.ano_mes.strftime("%b%y").lower()
+        self.file_path = f"ipca/source/data/ipca_{self.ano_mes_formatado}.csv"
+
+    def dataDownload(self):
+        """
+        Realiza o download dos dados em csv
+
+        Returns
+        ---
+        wget download
+        """
+        if os.path.exists(f"ipca/source/data/ipca_{self.ano_mes_formatado}.csv"):
+            return True
+        else:
+            return wget.download(
+                url=self.url, out=f"ipca/source/data/ipca_{self.ano_mes_formatado}.csv"
+            )
+
+    def dataTransform(self) -> DataFrame:
+        """
+        Realiza as transformações dos dados
+
+        Returns
+        ---
+        DataFrame
+        """
+
+        ipca_df: DataFrame = pd.read_csv(
+            self.file_path, sep=",", encoding="utf-8-sig", skiprows=2, low_memory=False
+        )
+
+        column_rename: dict[str, str] = {
+            "Unnamed: 1": "Território",
+            "IPCA - Número-índice (base: dezembro de 1993 = 100) (Número-índice)": "Número-índice",
+            "IPCA - Variação mensal (%)": "Variação mensal (%)",
+            "IPCA - Variação acumulada em 3 meses (%)": "Variação 3 meses (%)",
+            "IPCA - Variação acumulada em 6 meses (%)": "Variação 6 meses (%)",
+            "IPCA - Variação acumulada no ano (%)": "Variação anual (%)",
+            "IPCA - Variação acumulada em 12 meses (%)": "Variação 12 meses (%)",
+        }
+
+        ipca_df.rename(columns=column_rename, inplace=True)
+
+        # Substituindo as pontuações
+        ipca_df: DataFrame = ipca_df.replace("...", 0)
+
+        # Retirando as linhas de fonte e notas de rodapé
+        ipca_df: DataFrame = ipca_df.iloc[:-12]
+
+        meses: dict[str, int] = {
+            "janeiro": 1,
+            "fevereiro": 2,
+            "março": 3,
+            "abril": 4,
+            "maio": 5,
+            "junho": 6,
+            "julho": 7,
+            "agosto": 8,
+            "setembro": 9,
+            "outubro": 10,
+            "novembro": 11,
+            "dezembro": 12,
+        }
+
+        # Dividindo a coluna de Mês para obter informação do Ano
+        ipca_df[["Mês", "Ano"]] = ipca_df["Mês"].str.split(" ", expand=True)
+
+        # Preenchendo a coluna Número do Mês de acordo com as chaves da variavel mes
+        ipca_df["Número do Mês"] = ""
+        ipca_df["Número do Mês"] = ipca_df["Mês"].map(meses)
+
+        # Escolhendo colunas a serem visualizadas
+        ipca_df = ipca_df[
+            [
+                "Ano",
+                "Número do Mês",
+                "Mês",
+                "Número-índice",
+                "Variação mensal (%)",
+                "Variação 3 meses (%)",
+                "Variação 6 meses (%)",
+                "Variação anual (%)",
+                "Variação 12 meses (%)",
+            ]
+        ]
+
+        ipca_df["Ano"] = ipca_df["Ano"].astype(int)
+
+        ipca_df[
+            [
+                "Número-índice",
+                "Variação mensal (%)",
+                "Variação 3 meses (%)",
+                "Variação 6 meses (%)",
+                "Variação anual (%)",
+                "Variação 12 meses (%)",
+            ]
+        ] = ipca_df[
+            [
+                "Número-índice",
+                "Variação mensal (%)",
+                "Variação 3 meses (%)",
+                "Variação 6 meses (%)",
+                "Variação anual (%)",
+                "Variação 12 meses (%)",
+            ]
+        ].astype(
+            float
+        )
+
+        ipca_df.to_csv(
+            f"output/ipca_formatado_{self.ano_mes_formatado}.csv",
+            index=False,
+            decimal=",",
+            encoding="utf-8-sig",
+            sep=";",
+        )
+
+        return ipca_df
---
+++ ipca/source/view.py
@@ -0,0 +1,86 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+from pandas import DataFrame
+from .transform import dataFormat
+
+
+def data_copy() -> DataFrame:
+    """
+    Copia o DataFrame e retorna a cópia filtrada
+
+    Returns
+    ---
+    pandas.DataFrame
+    """
+    df_original: DataFrame = dataFormat().dataTransform()
+    df_copy: DataFrame = df_original.copy()
+
+    # Realizando um filtro de ano acima de 1999 e apenas o mês de Dezembro
+    df_copy = df_copy[
+        (df_copy["Ano"] > 1999) & (df_copy["Mês"].str.contains("dezembro"))
+    ]
+    return df_copy
+
+
+def view_graph():
+    """
+    Visualizando graficamente os dados finais
+
+    Returns
+    ---
+    `plotly`: `graph`
+    """
+
+    df_copy: DataFrame = data_copy()
+    # Utilizando algumas features do Plotly para criar um gráfico
+
+    # Passando o DataFrame como fonte de dados e escolhendo os eixos devidos
+    fig = px.bar(df_copy, x="Ano", y="Variação anual (%)", text_auto=".1f")
+
+    # Definindo os rótulos de dados como fonte 7 e alocando fora das colunas
+    fig.update_traces(
+        textfont_size=7,
+        textangle=0,
+        textposition="outside",
+        marker_color="midnightblue",
+    )
+
+    # Ajustando o gráfico -> título, centralizando e formatando eixos
+    fig.update_layout(
+        title={
+            "text": "Inflação nos últimos anos <br><sup>Variação anual, em %",
+            "y": 0.95,
+            "x": 0.5,
+            "xanchor": "center",
+            "yanchor": "top",
+            "font": {
+                "size": 20,
+                "color": "darkslategrey",
+                "family": "luminari, fantasy",
+            },
+        },
+        yaxis=dict(title="(%)", titlefont_size=12),
+        xaxis=dict(title="Período", titlefont_size=12, type="category"),
+        uniformtext_minsize=10,
+        uniformtext_mode="hide",
+        paper_bgcolor="rgb(255,255,255)",
+        plot_bgcolor="rgb(255,255,255)",
+    )
+
+    # Ajustando o ângulo do eixo X
+    fig.update_xaxes(tickangle=-60)
+
+    return fig
+
+
+def saving_graph():
+    """
+    Salvando o gráfico em formato de imagem
+
+    Returns
+    ---
+        image
+    """
+    fig = view_graph()
+    return fig.write_image("output/figura_final.jpeg", width=1280, height=720)
